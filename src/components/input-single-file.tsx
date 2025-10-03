9be8d2478a805b94f329e01c00191ecee5de5629
gustavolimapereira <gu.lim@hotmail.com>
2025-10-03 16:35:41 -0300

    update input single file

--- src/components/input-single-file.tsx
+++ src/components/input-single-file.tsx
@@ -1,88 +1,152 @@
 import { type VariantProps, tv } from "tailwind-variants";
 import Icon from "./icon";
 import Text, { textVariants } from "./text";
-import UploadFileIcon from '../assets/icons/upload-file.svg?react'
-import FileImageIcon from '../assets/icons/image.svg?react'
+import UploadFileIcon from "../assets/icons/upload-file.svg?react";
+import FileImageIcon from "../assets/icons/image.svg?react";
+import { useWatch } from "react-hook-form";
+import React from "react";
 
 export const inputSingleFileVariants = tv({
-    base: `
+  base: `
     flex flex-col items-center justify-center w-full
     border border-solid border-border-primary
   group-hover:border-border-active
     rounded-lg gap-1 transition
   `,
-    variants: {
-        size: {
-            md: "px-5 py-6",
-        },
+  variants: {
+    size: {
+      md: "px-5 py-6",
     },
-    defaultVariants: {
-        size: "md",
-    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
 });
 
 export const inputSingleFileIconVariants = tv({
-    base: "fill-placeholder",
-    variants: {
-        size: {
-            md: "w-8 h-8",
-        },
+  base: "fill-placeholder",
+  variants: {
+    size: {
+      md: "w-8 h-8",
     },
-    defaultVariants: {
-        size: "md",
-    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
 });
 
 interface InputSingleFileProps
-    extends VariantProps<typeof inputSingleFileVariants>,
+  extends VariantProps<typeof inputSingleFileVariants>,
     Omit<React.ComponentProps<"input">, "size"> {
-    error?: React.ReactNode;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    form: any;
-    allowedExtensions: string[];
-    maxFileSizeInMB: number;
-    replaceBy: React.ReactNode;
+  error?: React.ReactNode;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  form: any;
+  allowedExtensions?: string[];
+  maxFileSizeInMB: number;
+  replaceBy?: React.ReactNode;
 }
 
-export default function InputSingleFile({ size, error }: InputSingleFileProps) {
-    return (
-        <div>
-            <div className="w-full relative group cursor-pointer">
-                <input type="file" className="absolute top-0 right-0 w-full h-full opacity-0 cursor-pointer" />
-                <div className={inputSingleFileVariants()}>
-                    <Icon
-                        svg={UploadFileIcon}
-                        className={inputSingleFileIconVariants({ size })}
-                    />
-                    <Text variant="label-medium" className="text-placeholder text-center">
-                        Arraste o arquivo aqui <br />
-                        ou clique para selecionar
-                    </Text>
-                </div>
+export default function InputSingleFile({
+  size,
+  error,
+  form,
+  allowedExtensions,
+  maxFileSizeInMB,
+  ...props
+}: InputSingleFileProps) {
+  const formValues = useWatch({ control: form.control });
+  const name = props.name || "";
+  const formFile: File = React.useMemo(
+    () => formValues[name]?.[0],
+    [formValues, name]
+  );
+
+  const { fileExtensions, fileSize } = React.useMemo(
+    () => ({
+      fileExtensions: formFile?.name?.split(".").pop()?.toLowerCase() || "",
+      fileSize: formFile?.size || 0,
+    }),
+    [formFile]
+  );
+
+  function isValidExtension() {
+    return allowedExtensions?.includes(fileExtensions);
+  }
+
+  function isValidSize() {
+    return fileSize <= maxFileSizeInMB * 1024 * 1024;
+  }
+
+  function isValidFile() {
+    return isValidExtension() && isValidSize();
+  }
+
+  return (
+    <div>
+      {!formFile || !isValidFile ? (
+        <>
+          <div className="w-full relative group cursor-pointer">
+            <input
+              type="file"
+              className="absolute top-0 right-0 w-full h-full opacity-0 cursor-pointer"
+              {...props}
+            />
+            <div className={inputSingleFileVariants()}>
+              <Icon
+                svg={UploadFileIcon}
+                className={inputSingleFileIconVariants({ size })}
+              />
+              <Text
+                variant="label-medium"
+                className="text-placeholder text-center"
+              >
+                Arraste o arquivo aqui <br />
+                ou clique para selecionar
+              </Text>
             </div>
-            {error && (
-                <Text variant="label-small" className="text-accent-red">
-                    Erro no campo
-                </Text>
+          </div>
+          <div className="flez flex-col gap-1 mt-1">
+            {formFile && !isValidExtension() && (
+              <Text variant="label-small" className="text-accent-red">
+                Tipo de arquivo inválido.
+              </Text>
             )}
-            <div className="flex gap-3 items-center border border-solid border-border-primary mt-5 p-3 rounded">
-                <Icon svg={FileImageIcon} className="fill-white w-6 h-6" />
-                <div className="flex flex-col">
-                    <div className="truncate max-w-80">
-                        <Text variant="label-medium" className="text-placeholder">
-                            Nome do arquivo.png
-                        </Text>
-                    </div>
-                    <div className="flex">
-                        <button type="button" 
-                        className={
-                            textVariants({ 
-                                variant: "label-small",
-                                className: "text-accent-red cursor-pointer hover:underline"
-                                })}>Remover</button>
-                    </div>
-                </div>
+            {formFile && !isValidSize() && (
+              <Text variant="label-small" className="text-accent-red">
+                Tamanho do arquivo ultrapassa o maximo
+              </Text>
+            )}
+            {error && (
+              <Text variant="label-small" className="text-accent-red">
+                {error}
+              </Text>
+            )}{" "}
+          </div>
+        </>
+      ) : (
+        <div className="flex gap-3 items-center border border-solid border-border-primary mt-5 p-3 rounded">
+          <Icon svg={FileImageIcon} className="fill-white w-6 h-6" />
+          <div className="flex flex-col">
+            <div className="truncate max-w-80">
+              <Text variant="label-medium" className="text-placeholder">
+                {formFile.name}
+              </Text>
             </div>
+            <div className="flex">
+              <button
+                type="button"
+                className={textVariants({
+                  variant: "label-small",
+                  className: "text-accent-red cursor-pointer hover:underline",
+                })}
+                onClick={() => form.setValue(name, undefined)}
+              >
+                Remover
+              </button>
+            </div>
+          </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
