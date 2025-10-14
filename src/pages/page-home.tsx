31411e004658e850361451f33992beb7344718aa
gustavolimapereira <gu.lim@hotmail.com>
2025-10-14 09:06:16 -0300

    update project

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,13 +1,12 @@
 import Container from "../components/container";
-import PhotoWidget from "../contexts/photos/components/photo-widget";
-import type { Photo } from "../contexts/photos/models/photo";
+import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-9">
-        <PhotoWidget
-          photo={{
+      <PhotosList
+        photos={[
+          {
             id: "1",
             title: "Foto de um gato",
             imageId: "portrait-tower.png",
@@ -16,10 +15,19 @@ export default function PageHome() {
               { id: "2", title: "Cachorro" },
               { id: "3", title: "Papagaio" },
             ],
-          }}
-        />
-        <PhotoWidget photo={{} as Photo} loading />
-      </div>
+          },
+          {
+            id: "2",
+            title: "Foto de um gato",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "1", title: "Gatos" },
+              { id: "2", title: "Cachorro" },
+              { id: "3", title: "Papagaio" },
+            ],
+          },
+        ]}
+      />
     </Container>
   );
 }
