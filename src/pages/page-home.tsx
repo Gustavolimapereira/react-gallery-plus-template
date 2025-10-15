92494e84192bc3178e42f5d476114b95caf64c18
gustavolimapereira <gu.lim@hotmail.com>
2025-10-15 08:53:56 -0300

    update

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,18 @@
 import Container from "../components/container";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
 import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
+      <AlbumsFilter
+        albums={[
+          { id: "1", title: "Gatos" },
+          { id: "2", title: "Cachorro" },
+          { id: "3", title: "Papagaio" },
+        ]}
+        className="mb-9"
+      />
       <PhotosList
         photos={[
           {
