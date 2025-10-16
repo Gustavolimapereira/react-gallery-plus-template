81c766d80e1cfb54734bf06af827b1a0993cf32c
gustavolimapereira <gu.lim@hotmail.com>
2025-10-16 15:04:52 -0300

    update project

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -1,31 +1,36 @@
 import { Link } from "react-router";
 import Container from "./container";
-import Logo from "../assets/images/galeria-plus-full-logo.svg?react"
-import cx from "classnames"
+import Logo from "../assets/images/galeria-plus-full-logo.svg?react";
+import cx from "classnames";
 import Button from "./button";
 import PhotosSearch from "./photos-search";
 import Divider from "./divider";
+import PhotoNewDialog from "../contexts/photos/components/photo-new-dialog";
+import AlbumNewDialog from "../contexts/photos/components/album-new-dialog";
 
-interface MainHeaderProps extends React.ComponentProps<typeof Container> { }
+interface MainHeaderProps extends React.ComponentProps<typeof Container> {}
 
 export default function MainHeader({ className, ...props }: MainHeaderProps) {
-    return (
-        <Container
-            as="header"
-            className={cx("flex justify-between items-center gap-10", className)}
-            {...props}
-        >
-            <Link to="/">
-                <Logo className="h-5" />
-            </Link>
+  return (
+    <Container
+      as="header"
+      className={cx("flex justify-between items-center gap-10", className)}
+      {...props}
+    >
+      <Link to="/">
+        <Logo className="h-5" />
+      </Link>
 
-            <PhotosSearch />
-            <Divider orientation="vertical" className="h-10" />
+      <PhotosSearch />
+      <Divider orientation="vertical" className="h-10" />
 
-            <div className="flex items-center gap-3">
-                <Button>Nova foto</Button>
-                <Button variant="secondary">Criar álbum</Button>
-            </div>
-        </Container >
-    )
+      <div className="flex items-center gap-3">
+        <PhotoNewDialog trigger={<Button>Nova foto</Button>} />
+
+        <AlbumNewDialog
+          trigger={<Button variant="secondary">Criar álbum</Button>}
+        />
+      </div>
+    </Container>
+  );
 }
